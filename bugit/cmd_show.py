4eb6857a7fae426883c78acbb2bb167b139fb7a2
Tommi Virtanen <tv@eagain.net>
2007-12-14 00:18:10 +0200

    Implement "bugit show".

--- bugit/cmd_show.py
+++ bugit/cmd_show.py
@@ -1,9 +1,14 @@
 import optparse
+import os
+import textwrap
+
+from bugit import storage
+from bugit import tagsort
 
 def main(args):
     """Show details of a ticket"""
     parser = optparse.OptionParser(
-        usage='%prog show [OPTS] [--] [TICKET..]',
+        usage='%prog show [OPTS] [--] [TICKET]',
         )
     parser.add_option(
         '-v', '--verbose',
@@ -14,15 +19,99 @@ def main(args):
         '--format',
         help='show output in this format',
         )
-    parser.add_option(
-        '--variable',
-        help='display only this variable',
-        metavar='NAME',
-        action='append',
-        )
     (options, args) = parser.parse_args(args)
 
-    raise NotImplementedError
-    if not args:
-        parser.print_help()
+    if options.verbose:
+        raise NotImplementedError
+    if options.format:
+        raise NotImplementedError
 
+    if not args:
+        # TODO check for stored default ticket
+        parser.error('no default ticket set')
+
+    if len(args) > 1:
+        parser.error('too many arguments')
+
+    (requested_ticket,) = args
+
+    # TODO fetch by sha
+    assert requested_ticket.startswith('#')
+
+    def list_tickets():
+        # TODO share me
+        for (mode, type_, object, basename) in storage.git_ls_tree(
+            path='',
+            children=True,
+            ):
+            yield basename
+
+    for ticket in list_tickets():
+        # TODO fetch by name
+
+        number = storage.get(os.path.join(ticket, 'number')).rstrip()
+        if '#%s' % number == requested_ticket:
+            print 'ticket %s' % ticket
+            print 'number #%s' % number
+            tags = set(storage.ls(os.path.join(ticket, 'tags')))
+            tags = tagsort.human_friendly_tagsort(tags)
+            print textwrap.fill(
+                ' '.join(tags),
+                initial_indent='tags ',
+                subsequent_indent='     ',
+                break_long_words=False,
+                )
+            print 'seen build/301' #TODO
+            print
+            title = storage.get(os.path.join(ticket, 'title')).rstrip()
+            print '\t%s' % title
+            print
+            description = storage.get(os.path.join(ticket, 'description')).rstrip()
+            if description is not None:
+                print '\n'.join([
+                        '\t%s' % line
+                        for line in description.split('\n')
+                        ])
+            print
+            def get_the_rest():
+                for name in storage.ls(ticket):
+                    if name in [
+                        'number',
+                        'title',
+                        'description',
+                        ]:
+                        continue
+                    leading = name.split(os.sep, 1)[0]
+                    if leading in [
+                        'tags',
+                        'seen',
+                        'not-seen',
+                        ]:
+                        continue
+                    yield name
+            the_rest = sorted(get_the_rest())
+            if the_rest:
+                for name in the_rest:
+                    content = storage.get(os.path.join(ticket, name)).rstrip()
+                    if not content:
+                        print name
+                    elif '\n' not in content:
+                        # one line with optional newline
+                        oneline = '%s=%s' % (name, content)
+                        if len(oneline) <= 70:
+                            # short enough to fit on one line
+                            print oneline
+                        else:
+                            print '%s=' % name
+                            print '\n'.join(textwrap.wrap(
+                                content,
+                                initial_indent='\t',
+                                subsequent_indent='\t',
+                                break_long_words=False,
+                                ))
+                    else:
+                        print '%s=' % name
+                        print '\n'.join([
+                                '\t%s' % line
+                                for line in content.split('\n')
+                                ])
--- bugit/test/test_cmd_show.py
+++ bugit/test/test_cmd_show.py
@@ -1,4 +1,8 @@
+from __future__ import with_statement
+
 from nose.tools import eq_ as eq
+
+from bugit import storage
 
 from bugit.test import util
 
@@ -10,11 +14,117 @@ def test_help():
             ],
         )
     eq(result.stdout, """\
-Usage: bugit show [OPTS] [--] [TICKET..]
+Usage: bugit show [OPTS] [--] [TICKET]
 
 Options:
   -h, --help       show this help message and exit
   -v, --verbose    show more information
   --format=FORMAT  show output in this format
-  --variable=NAME  display only this variable
 """)
+
+def test_bad_no_default_ticket():
+    result = util.clitest(
+        args=[
+            'show',
+            ],
+        exit_status=2,
+        allow_stderr=True,
+        )
+    eq(result.stdout, '')
+    eq(result.stderr, """\
+Usage: bugit show [OPTS] [--] [TICKET]
+
+bugit: error: no default ticket set
+""")
+
+def test_bad_too_many_args():
+    result = util.clitest(
+        args=[
+            'show',
+            'foo',
+            'bar',
+            ],
+        exit_status=2,
+        allow_stderr=True,
+        )
+    eq(result.stdout, '')
+    eq(result.stderr, """\
+Usage: bugit show [OPTS] [--] [TICKET]
+
+bugit: error: too many arguments
+""")
+
+def test_simple():
+    tmp = util.maketemp()
+    storage.git_init(tmp)
+    storage.init(tmp)
+    with storage.Transaction(tmp) as t:
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/number',
+            '3431\n',
+            )
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/title',
+            'Oncolator segfaults on some inputs\n',
+            )
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/tags/priority:high',
+            '',
+            )
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/tags/denial-of-service',
+            '',
+            )
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/tags/security',
+            '',
+            )
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/tags/reporter:jdoe@example.com',
+            '',
+            )
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/description',
+            """\
+The Oncolator service segfaults if I go to the web page,
+login, choose quick oncolation from the radio buttons and
+click the "Onc!" button.
+
+I need to demo this to the Board of Directors on Monday, need a
+fix quick! It crashed on me today around 9:20 am, you should be
+able to find it in the server logs.
+""",
+            )
+        t.set(
+            'd239371f3b6b61ca1076bb460e331b3edb412970/browser',
+            "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)",
+            )
+    result = util.clitest(
+        args=[
+            'show',
+            '#3431',
+            ],
+        cwd=tmp,
+        )
+    eq(result.stdout, """\
+ticket d239371f3b6b61ca1076bb460e331b3edb412970
+number #3431
+tags priority:high denial-of-service security
+     reporter:jdoe@example.com
+seen build/301
+
+\tOncolator segfaults on some inputs
+
+\tThe Oncolator service segfaults if I go to the web page,
+\tlogin, choose quick oncolation from the radio buttons and
+\tclick the "Onc!" button.
+\t
+\tI need to demo this to the Board of Directors on Monday, need a
+\tfix quick! It crashed on me today around 9:20 am, you should be
+\table to find it in the server logs.
+
+browser=
+\tMozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20061201
+\tFirefox/2.0.0.6 (Ubuntu-feisty)
+""",
+       'stdout does not match:\n%s' % result.stdout)
